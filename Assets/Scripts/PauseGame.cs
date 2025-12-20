532416e0de4cffc2b1892b059cd08b1c3912d558
CrypticCapricorn <cryptic.artwork@gmail.com>
2025-12-20 16:18:44 +0100

    Anims and pause menu
    Pause menu no workie

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenuCanvas;
+    private bool isPaused = false;
+
+    private void Start()
+    {
+        ResumeGame();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void PauseGameWoutCanvas()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+        if (pauseMenuCanvas != null)
+            pauseMenuCanvas.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        if (pauseMenuCanvas != null)
+            pauseMenuCanvas.SetActive(false);
+    }
+}
