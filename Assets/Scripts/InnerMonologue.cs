6043362029bf5e9a022c3901f0eba7814e5c8764
MarkusSecundus <kubahronik.g@gmail.com>
2025-12-21 12:19:03 +0100

    chapter 2 fixes

--- Assets/Scripts/InnerMonologue.cs
+++ Assets/Scripts/InnerMonologue.cs
@@ -5,6 +5,7 @@ using UnityEngine.Events;
 
 public class InnerMonologuePlayer : MonoBehaviour
 {
+	[SerializeField] bool OnlyOnce = true;
 	[SerializeField] DialogBubble _target;
 	[Multiline][SerializeField] string _text;
 	[SerializeField] UnityEvent _onFinished;
@@ -13,8 +14,11 @@ public class InnerMonologuePlayer : MonoBehaviour
 
 	float? ifNonzero(float f) => f <= 0 ? null : f;
 
+	bool _didPlay = false;
 	public void DoPlay()
 	{
+		if (OnlyOnce && _didPlay) return;
+		_didPlay = true;
 		_target.StartPrintout(_text, true, _onFinished.Invoke,
 			ifNonzero(_bubbleDuration),
 			ifNonzero(_secondsPerChar)
